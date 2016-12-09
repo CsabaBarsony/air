01c9dd167f57ba4d3dea329ad3756e3905b490f4
= <=>
2016-12-09 07:49:14 +0100

    promise

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,5 +1,10 @@
+var loadcounter = 0;
+var readycounter = 0;
+
 var foodSuggest = new Suggest(document.getElementById('suggest-container'), function(text, resolve) {
+    console.log('load', loadcounter++);
     setTimeout(function() {
+        console.log('ready', readycounter++);
         resolve(['cate', 'doge', 'ape']);
-    }, 300);
+    }, 600);
 });
