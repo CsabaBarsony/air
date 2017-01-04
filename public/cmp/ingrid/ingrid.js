07c8e5d75f1878cf5ce50546eb484d6be7877d9b
Csaba Barsony <v-cbarsony@hotels.com>
2017-01-04 16:56:58 +0100

    bella

--- public/cmp/ingrid/ingrid.js
+++ public/cmp/ingrid/ingrid.js
@@ -1,14 +1,8 @@
 'use strict';
 //globals: scion
 
-function Ingrid(container, ingredients, onChange) {
-    var model = {
-        ingredients: ingredients,
-        update: function(ingredients) {
-            this.ingredients = ingredients;
-            render();
-        }
-    };
+function Ingrid(container, originalIngredients, onChange) {
+    var ingredients = bella.immutable.deepClone(originalIngredients);
 
     var chart = {
         states: [
@@ -41,7 +35,7 @@ function Ingrid(container, ingredients, onChange) {
     sc.start();
 
     function onListingEntry(action) {
-        model.update();
+        render();
     }
 
     function onEditingEntry(action) {
@@ -54,12 +48,11 @@ function Ingrid(container, ingredients, onChange) {
 
     function addClick(e) {
         var amount = parseInt(e.target.parentElement.querySelector('input').value);
-        var ingredient = model.ingredients[e.target.parentElement.dataset.index];
+        var ingredient = ingredients[e.target.parentElement.dataset.index];
         ingredient.editing = false;
-        ingredient.amount = amount;
-        container.innerHTML = '';
-        container.appendChild(render());
-        var ingredientResult = model.ingredients.map((ingredient) => {
+        ingredient.amount = amount || 0;
+        render();
+        var ingredientResult = ingredients.map((ingredient) => {
             return {
                 food: ingredient.food,
                 amount: ingredient.amount,
@@ -70,10 +63,9 @@ function Ingrid(container, ingredients, onChange) {
     }
 
     function removeClick(e) {
-        model.ingredients.splice(e.target.parentElement.dataset.index, 1);
-        container.innerHTML = '';
-        container.appendChild(render());
-        var ingredientResult = model.ingredients.map((ingredient) => {
+        ingredients.splice(e.target.parentElement.dataset.index, 1);
+        render();
+        var ingredientResult = ingredients.map((ingredient) => {
             return {
                 food: ingredient.food,
                 amount: ingredient.amount,
@@ -108,7 +100,7 @@ function Ingrid(container, ingredients, onChange) {
 
         var list = document.createElement('ul');
         var templateData = {
-            ingredients: model.ingredients,
+            ingredients: ingredients,
             units: Ingrid.units
         };
         var template = Handlebars.compile(templateString, templateData);
@@ -121,21 +113,20 @@ function Ingrid(container, ingredients, onChange) {
 
     this.add = function(food) {
         var ingredientList = container.querySelector('ul');
-        model.ingredients.forEach((ingredient, index) => {
+        ingredients.forEach((ingredient, index) => {
             if(ingredient.editing) {
                 var amount = parseInt(ingredientList.querySelector('li[data-index="' + index + '"] input').value);
                 ingredient.amount = isNaN(amount) ? 0 : amount;
             }
             ingredient.editing = false;
         });
-        model.ingredients.push({
+        ingredients.push({
             food: food,
             editing: true,
             amount: null,
             unit: Ingrid.units.G
         });
-        container.innerHTML = '';
-        container.appendChild(render());
+        render();
     };
 }
 
